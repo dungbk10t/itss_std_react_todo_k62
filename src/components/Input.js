b7e5dbc9d679468d68e2cac4d5667fb6a0822578
dungbk10t <dungbk10t@gmail.com>
2021-04-08 04:17:56 +0000

    Add new Todo

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,24 +4,26 @@ import React, { useState } from 'react';
 　・新しいTodoを作成するINPUTフィールドを作成するコンポーネント
 　・Enterをクリックされたら入力された文字を使って新しいTodoを作成する
 */
-function Input( onAdd ) {
+function Input( { onAdd } ) {
   const [text, setText] = React.useState('');
+
   const handleChange = e => setText(e.target.value);
-  
+
   const handleKeyDown = e => {
-    if(e.keyCode === 13) {
+    if (e.keyCode === 13) {
       onAdd(text);
       setText('');
     }
-  }
+  };
+
   return (
     <div className="panel-block">
       <input
         class="input"
-        text="text"
+        type="text"
         placeholder="Enter to add"
         value={text}
-        onChane={handleChange}
+        onChange={handleChange}
         onKeyDown={handleKeyDown}
       />
     </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -36,15 +36,15 @@ function Todo() {
     putItems(newItems);
   }
   const handleAdd = text => {
-    putItems([...items,{key: getKey(), text, done:false}]);
-  }
+    putItems([...items, { key: getKey(), text, done: false }]);
+  };
 
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
-      <Input onAdd={handleAdd}/>
+      <Input onAdd={handleAdd} />
       {items.map(item => (
         <TodoItem
           key={item.key}
