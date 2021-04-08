956f0dfa4a08c3721fe9eae4dbc6b2baa9efe756
dungbk10t <dungbk10t@gmail.com>
2021-04-08 03:50:24 +0000

    Change Todo status

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -26,6 +26,15 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
+  const handleCheck = checked => {
+    const newItems = items.map(item => {
+      if(item.key===checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  }
   const handleAdd = text => {
     putItems([...items,{key: getKey(), text, done:false}]);
   }
@@ -40,6 +49,7 @@ function Todo() {
         <TodoItem
           key={item.key}
           item={item}
+          onCheck={handleCheck}
         />
         
       ))}
